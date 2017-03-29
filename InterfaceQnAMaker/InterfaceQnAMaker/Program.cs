6ac6f7def7952679b81d1a04417a7825f19c1b07
Amy Kate Nicholson <amykate_7@live.com>
2017-03-29 15:22:06 +0100

    Added QnA maker patch request to add new knowledge

--- InterfaceQnAMaker/InterfaceQnAMaker/Program.cs
+++ InterfaceQnAMaker/InterfaceQnAMaker/Program.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using System.Configuration;
 using Newtonsoft.Json;
 using System.Net;
+using System.Net.Http;
+using System.IO;
+
+using InterfaceQnAMaker;
 
 namespace InterfaceQnAMaker
 {
@@ -33,6 +37,8 @@ namespace InterfaceQnAMaker
             var exit = false;
             string output = string.Empty;
             string query = string.Empty;
+            string question = string.Empty;
+            string answer = string.Empty;
 
             try
             {
@@ -55,8 +61,15 @@ namespace InterfaceQnAMaker
                         await Run();
                         break;
                     case '2':
-                        
 
+                        Console.WriteLine("Please enter a new question:");
+                        question = Console.ReadLine();
+                        Console.WriteLine("Please enter a new answer:");
+                        answer = Console.ReadLine();
+
+                        output = addQnA(knowledgeBaseID, qnaMakerSubscriptionKey, question, answer);
+
+                        Console.WriteLine(output);
                         await Run();
                         break;
                     
@@ -117,6 +130,40 @@ namespace InterfaceQnAMaker
             }
 
             response = "answer: " + answer + " Score: " + score;
+            return response;
+        }
+
+        public static string addQnA(string id, string key, string question, string answer)
+        {
+            string response = string.Empty;
+            string query = "{\"add\": {\"qnaPairs\": [{\"answer\": \"" + answer + "\", \"question\": \"" + question + "\"}]}}";
+
+            var client = new HttpClient();
+
+            // Build URI
+            var uri = "https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/" + id;
+
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(uri);
+            request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+            request.Method = "PATCH";
+            request.ContentType = "application/json";
+           
+
+            System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+            byte[] bytes = encoding.GetBytes(query);
+            request.ContentLength = bytes.Length;
+
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                // Send the data.
+                requestStream.Write(bytes, 0, bytes.Length);
+            }
+            var responseRaw = (HttpWebResponse)request.GetResponse();
+            response = new StreamReader(responseRaw.GetResponseStream()).ReadToEnd();
+
+
+
             return response;
         }
     }
