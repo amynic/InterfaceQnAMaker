e241423f322cbfa013be9351aa871f96544d8df9
Amy Kate Nicholson <amykate_7@live.com>
2017-03-29 16:54:44 +0100

    Move addQnA method to own class

--- InterfaceQnAMaker/InterfaceQnAMaker/AddToKnowledge.cs
+++ InterfaceQnAMaker/InterfaceQnAMaker/AddToKnowledge.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +13,38 @@ namespace InterfaceQnAMaker
     {
         //If the sample query does not return the correct knowledge, add it to the service
 
-        public void addQnA()
+        public static string addQnA(string id, string key, string question, string answer)
         {
+            string response = string.Empty;
+            string query = "{\"add\": {\"qnaPairs\": [{\"answer\": \"" + answer + "\", \"question\": \"" + question + "\"}]}}";
 
+            var client = new HttpClient();
+
+            // Build URI
+            var uri = Program.uriV2 + id;
+
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(uri);
+            request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+            request.Method = "PATCH";
+            request.ContentType = "application/json";
+
+
+            System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+            byte[] bytes = encoding.GetBytes(query);
+            request.ContentLength = bytes.Length;
+
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                // Send the data.
+                requestStream.Write(bytes, 0, bytes.Length);
+            }
+            var responseRaw = (HttpWebResponse)request.GetResponse();
+            response = new StreamReader(responseRaw.GetResponseStream()).ReadToEnd();
+
+
+
+            return response;
         }
     }
 }
--- InterfaceQnAMaker/InterfaceQnAMaker/Program.cs
+++ InterfaceQnAMaker/InterfaceQnAMaker/Program.cs
@@ -70,7 +70,7 @@ namespace InterfaceQnAMaker
                         Console.WriteLine("Please enter a new answer:");
                         answer = Console.ReadLine();
 
-                        output = addQnA(knowledgeBaseID, qnaMakerSubscriptionKey, question, answer);
+                        output = AddToKnowledge.addQnA(knowledgeBaseID, qnaMakerSubscriptionKey, question, answer);
 
                         Console.WriteLine(output);
                         await Run();
@@ -105,40 +105,7 @@ namespace InterfaceQnAMaker
         }
 
 
-        public static string addQnA(string id, string key, string question, string answer)
-        {
-            string response = string.Empty;
-            string query = "{\"add\": {\"qnaPairs\": [{\"answer\": \"" + answer + "\", \"question\": \"" + question + "\"}]}}";
-
-            var client = new HttpClient();
-
-            // Build URI
-            var uri = uriV2 + id;
-
-
-            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(uri);
-            request.Headers.Add("Ocp-Apim-Subscription-Key", key);
-            request.Method = "PATCH";
-            request.ContentType = "application/json";
-           
-
-            System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
-            byte[] bytes = encoding.GetBytes(query);
-            request.ContentLength = bytes.Length;
-
-            using (Stream requestStream = request.GetRequestStream())
-            {
-                // Send the data.
-                requestStream.Write(bytes, 0, bytes.Length);
-            }
-            var responseRaw = (HttpWebResponse)request.GetResponse();
-            response = new StreamReader(responseRaw.GetResponseStream()).ReadToEnd();
-
-
-
-            return response;
-        }
-
+        
         public static string publishQnA(string id, string key)
         {
 
